f864ea8296247396eae62a61524a422ace1e77ca
vnovikov <nik.hbb@gmail.com>
2024-02-25 08:34:40 +0200

    Add main point

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,11 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        FruitThread fruits = new FruitThread(5);
+        VegetableThread vegetables = new VegetableThread(5);
 
+        vegetables.setPriority(Thread.MAX_PRIORITY);
+        fruits.start();
+        vegetables.start();
     }
 }
